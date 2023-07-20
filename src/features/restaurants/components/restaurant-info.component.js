7f2ec20e4f801b52b7ed40c135111f22a27994d4
Kandekore <darren@kandekore.net>
2023-07-20 22:06:35 +0100

    spacer

--- src/features/restaurants/components/restaurant-info.component.js
+++ src/features/restaurants/components/restaurant-info.component.js
@@ -1,44 +1,96 @@
-import react from "react";
-import {Text, StyleSheet} from "react-native";
+import React from "react";
+import styled from "styled-components/native";
+import { Text, Image, View } from "react-native";
 import { Card } from "react-native-paper";
-import styled from "styled-components/native"
+import { SvgXml } from "react-native-svg";
+
+import { Spacer } from "../../../components/spacer/spacer.component";
+import star from "../../../../assets/star";
+import open from "../../../../assets/open";
 
 const RestaurantCard = styled(Card)`
-
-background-color: yellow;`;
-const Title = styled.Text`
-font-family: ${(props) => props.theme.fonts.body};
-padding: ${(props) => props.theme.space[2]};
-color: ${(props) => props.theme.colors.ui.primary};
-
+  background-color: ${(props) => props.theme.colors.bg.primary};
 `;
+
 const RestaurantCardCover = styled(Card.Cover)`
-background-color: ${(props) => props.theme.colors.bg.primary};
-padding: ${(props) => props.theme.space[2]};`
+  padding: ${(props) => props.theme.space[3]};
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
 
+const Address = styled(Text)`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+`;
 
-export const RestaurantInfo = ({restaurant ={}}) => {
-    const {
-        name = 'Some Restaurant',
-        icon,
-        photos = ["https://upload.wikimedia.org/wikipedia/commons/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg"],
-        address = "100 Some random street",
-        isOpenNow = true,
-        rating = 5,
-        isClosedTemporarily,
-    } = restaurant;
-    return (
-        <RestaurantCard >
-            <RestaurantCardCover source={{ uri: photos[0]}} />
-            <Title >{name}</Title>
-        </RestaurantCard>
-    )
-}
+const Title = styled(Text)`
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-size: ${(props) => props.theme.fontSizes.body};
+  color: ${(props) => props.theme.colors.ui.primary};
+`;
 
+const Info = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
 
+const Rating = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
+`;
 
-/*const styles = StyleSheet.create({
-    card: { backgroundColor: "white"},
-    cover: { padding: 20, backgroundColor: "white"},
-    title: { padding: 16},
-}) */
+const Section = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+const SectionEnd = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
+`;
+
+export const RestaurantInfoCard = ({ restaurant = {} }) => {
+  const {
+    name = "Some Restaurant",
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
+    photos = [
+      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+    ],
+    address = "100 some random street",
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily = true,
+  } = restaurant;
+
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
+
+  return (
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+      <Info>
+        <Title>{name}</Title>
+        <Section>
+          <Rating>
+            {ratingArray.map(() => (
+              <SvgXml xml={star} width={20} height={20} />
+            ))}
+          </Rating>
+          <SectionEnd>
+            {isClosedTemporarily && (
+              <Text variant="label" style={{ color: "red" }}>
+                CLOSED TEMPORARILY
+              </Text>
+            )}
+            <Spacer position="left" size="large">
+              {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+            </Spacer>
+            <Spacer position="left" size="large">
+              <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+            </Spacer>
+          </SectionEnd>
+        </Section>
+        <Address>{address}</Address>
+      </Info>
+    </RestaurantCard>
+  );
+};
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,54 +1,31 @@
 import React from "react";
 import { Searchbar } from "react-native-paper";
-import { StatusBar, StyleSheet, Text, View } from "react-native";
-import { RestaurantInfo } from "../components/restaurant-info.component";
-//import SafeAreaView from 'react-native-safe-area-view';
-import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
-import styled from "styled-components/native"
+import { StatusBar, StyleSheet, SafeAreaView, Text, View } from "react-native";
+import styled from "styled-components/native";
+
+import { RestaurantInfoCard } from "../components/restaurant-info.component";
 
 const SafeArea = styled(SafeAreaView)`
-flex: 1;
-${StatusBar.currentHeight && `marginTop: ${StatusBar.currentHeight}px`};
+  flex: 1;
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
 `;
 
-const SearchContainer = styled(View)`
-    padding: 16px;
+const SearchContainer = styled.View`
+  padding: ${(props) => props.theme.space[3]};
 `;
 
 const RestaurantListContainer = styled.View`
-flex: 1;
-padding: 16px;
-backgroundColor: red;
+  flex: 1;
+  padding: ${(props) => props.theme.space[3]};
 `;
 
-
 export const RestaurantsScreen = () => (
-    
-    <SafeArea>
-        <SearchContainer>
-          <Searchbar />
-        </SearchContainer>
-        <RestaurantListContainer >
-          
-          <RestaurantInfo />
-        </RestaurantListContainer>
-      </SafeArea>
-     
-      
+  <SafeArea>
+    <SearchContainer>
+      <Searchbar />
+    </SearchContainer>
+    <RestaurantListContainer>
+      <RestaurantInfoCard />
+    </RestaurantListContainer>
+  </SafeArea>
 );
-
-/*const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      marginTop: StatusBar.currentHeight
-    },
-    search: {
-      padding: 16
-    },
-    list: {
-      flex: 1,
-      padding: 16,
-      backgroundColor: "blue"
-    }
-  });*/
-  
